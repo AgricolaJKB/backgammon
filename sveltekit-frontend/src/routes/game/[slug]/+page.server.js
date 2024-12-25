ac6b33ab02fcccf972e884dadfbba2f728dd62d9
Jakob Bauer <jakob.bauer@zeit.de>
2024-12-25 21:33:39 +0100

    backend cleanup

--- sveltekit-frontend/src/routes/game/[slug]/+page.server.js
+++ sveltekit-frontend/src/routes/game/[slug]/+page.server.js
@@ -1,21 +1,3 @@
-import * as auth from "$lib/server/auth";
-import { fail, redirect } from "@sveltejs/kit";
+import * as setup from "../../+page.server.js";
 
-export const load = async (event) => {
-  if (!event.locals.user) {
-    return redirect(302, "/login");
-  }
-  return { ...event.locals };
-};
-
-export const actions = {
-  logout: async (event) => {
-    if (!event.locals.session) {
-      return fail(401);
-    }
-    delete event.locals.user;
-    auth.deleteSessionTokenCookie(event);
-
-    return redirect(302, "/login");
-  }
-};
+export default setup;
